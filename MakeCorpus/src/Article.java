5585cc843f2a3e28e532d83e1b8869309f836d5d
georgethebeatle@gmail.com <georgethebeatle@gmail.com@2e92c052-a7f7-af04-06a8-936502c982f6>
2011-06-24 20:45:28 +0000

    Add corpus utilities

---
+++ MakeCorpus/src/Article.java
@@ -0,0 +1,29 @@
+
+public class Article {
+
+	private int id;
+	private String text;
+
+	public Article(int id, String text) {
+		super();
+		this.id = id;
+		this.text = text;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
